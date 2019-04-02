4e04263827727ea4d8bd6a7db5fc5732489f7b9f
AlexGLovett <alexanderglovett@gmail.com>
2019-04-01 21:34:54 -0400

    made pages

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,49 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  // routes here
+  app.get("/api/users", function(req, res) {
+    db.user.findAll({}).then(function(dbuser) {
+      res.json(dbuser);
+    });
+  });
+
+  app.get("/api/users/:id", function(req, res) {
+    // Find one user with the id in req.params.id and return them to the user with res.json
+    db.user
+      .findOne({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function(dbuser) {
+        res.json(dbuser);
+      });
+  });
+
+  app.post("/api/users", function(req, res) {
+    console.log(req.body);
+    db.user
+      .create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+      })
+      .then(function(dbusers) {
+        res.json(dbusers);
+      });
+  });
+
+  app.delete("/api/users/:id", function(req, res) {
+    // Delete the user with the id available to us in req.params.id
+    db.user
+      .destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function(dbuser) {
+        res.json(dbuser);
+      });
+  });
+};
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,30 @@
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  //handlebars routing
+  app.get("/", (req, res) => {
+    res.render("index", { title: "Home Page" });
+  });
+
+  app.get("/createdash", (req, res) => {
+    res.render("createdash", { title: "Create Dash", create: true });
+  });
+
+  app.get("/dashboard", (req, res) => {
+    res.render("dashboard", { title: "Dashboard", create: true });
+  });
+
+  app.get("/user", (req, res) => {
+    res.render("user", { title: "User" });
+  });
+
+  app.get("/datasets", (req, res) => {
+    res.render("datasets", { title: "My Datasets" });
+  });
+};
